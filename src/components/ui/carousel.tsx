919da16e3ca96dcf8850940eb9dc41e772343a9d
Will <willwow@outlook.com>
2024-01-07 02:17:53 -0300

    Fix: Embla import

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,15 +1,16 @@
 import * as React from "react"
-import useEmblaCarousel, {
-  type UseEmblaCarouselType as CarouselApi,
+import {
+  type EmblaCarouselType as CarouselApi,
   type EmblaOptionsType as CarouselOptions,
   type EmblaPluginType as CarouselPlugin,
-} from "embla-carousel-react"
-
+} from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
 
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+
 
 type CarouselProps = {
   opts?: CarouselOptions
